fce1adad34bdb3d1229a006e99d1c64bcf795c39
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-11-22 14:07:16 +0700

    Membuat Row agar selebar layar dan dengan padding, verticalAligment

--- app/src/main/java/com/example/petemuan8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/petemuan8/ui/screen/RencanaStudyView.kt
@@ -2,14 +2,19 @@ package com.example.petemuan8.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.petemuan8.R
 import com.example.petemuan8.model.Mahasiswa
 import com.example.petemuan8.model.RencanaStudy
@@ -33,6 +38,12 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ){
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
 
     }
 
