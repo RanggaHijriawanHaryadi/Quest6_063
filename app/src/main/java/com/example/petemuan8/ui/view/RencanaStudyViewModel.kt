59e772220caaa96172fc2efd6e6f1b0298694427
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-11-22 15:21:01 +0700

    Membuat fungsi setMataKuliah dan setKelas

--- app/src/main/java/com/example/petemuan8/ui/view/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/petemuan8/ui/view/RencanaStudyViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.petemuan8.ui.view
 
+import com.example.petemuan8.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class RencanaStudyViewModel {
+    private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi : StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK ->
+            stateMK.copy(mataKuliah = mkPilihan)
+        }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+
 }
