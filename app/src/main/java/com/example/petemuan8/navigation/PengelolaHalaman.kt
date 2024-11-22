c8a392d8e535494953540633d0783e5d62a860c0
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-11-22 15:34:40 +0700

    Membuat fungsi MahasiswaApp

--- app/src/main/java/com/example/petemuan8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/petemuan8/navigation/PengelolaHalaman.kt
@@ -1,6 +1,14 @@
 package com.example.petemuan8.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import com.example.petemuan8.model.Mahasiswa
+import com.example.petemuan8.ui.view.MahasiswaViewModel
+import com.example.petemuan8.ui.view.RencanaStudyViewModel
 
 enum class Halaman {
     Splash,
@@ -9,3 +17,12 @@ enum class Halaman {
     Tampil
 }
 
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+
+}
